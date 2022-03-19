bd2fb7c0d7a06d3613ce1e77ecd7cc4bad2d8e94
Muhammad Soban <fa18-bcs-058@cuilahore.edu.pk>
2022-03-19 20:53:13 +0500

    geography changes cities pagination removed location pagination removed location data fixed

--- app/controllers/geography/cities.controller.js
+++ app/controllers/geography/cities.controller.js
@@ -9,16 +9,9 @@ import { Cities } from "../../models";
 export const getAllcities = async (req, res) => {
   try {
     const { page = 1, limit = 10, sort = "key" } = req?.query;
-    const cities = await Cities.find(req.body.query)
-      .limit(+limit * 1)
-      .skip((+page - 1) * +limit)
-      .sort(sort);
+    const cities = await Cities.find(req.body.query).sort(sort);
     res.status(200).json({
       count: cities.length,
-      page,
-      totalPages: Math.ceil(
-        (await Cities.countDocuments(req.body.query)) / +limit
-      ),
       data: cities,
     });
   } catch (error) {
--- app/controllers/geography/locations.controller.js
+++ app/controllers/geography/locations.controller.js
@@ -8,21 +8,15 @@ import { Location } from "../../models";
 
 export const getLocations = async (req, res) => {
   try {
-    const { page = 1, limit = 10, sort = "key" } = req.query;
-    const { city_id = 1 } = req.body?.query;
+    const { sort = "key", city_id = 1 } = req.query;
 
     if (!city_id || isNaN(city_id)) {
       return res.status(400).json({ message: "Invalid City id" });
     }
-    const locations = await Location.find({ city: city_id })
-      .limit(+limit * 1)
-      .skip((+page - 1) * +limit)
-      .sort(sort);
+    const locations = await Location.find({ city: city_id }).sort(sort);
 
     res.status(200).json({
       count: locations.length,
-      page,
-      totalPages: Math.ceil((await Location.countDocuments()) / +limit),
       data: locations,
     });
   } catch (error) {
